32c29428af00c574680c2d237fba3a31290536b1
Dru1DD <mrtproger@gmail.com>
2025-11-21 21:27:07 +0100

    update: buttons ui

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -12,33 +12,35 @@ const Footer = ({
   handleUpButtonClicked,
 }: FooterProps) => {
   return (
-    <div className="w-full p-4 bottom-0 flex justify-evenly bg-accent-blue border-t-2 border-black">
-      <button
-        onClick={handleLeftButtonClicked}
-        className="bg-white hover:bg-gray-200 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
-      >
-        ←
-      </button>
+    <div className="w-full p-4 bottom-0 flex justify-center items-center bg-accent-blue border-t-2 border-black">
+      <div className="w-full max-w-lg flex justify-evenly">
+        <button
+          onClick={handleLeftButtonClicked}
+          className="bg-white hover:bg-gray-200 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
+        >
+          ←
+        </button>
 
-      <button
-        onClick={handleUpButtonClicked}
-        className="bg-white hover:bg-gray-200 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
-      >
-        ↑
-      </button>
-      <button
-        onClick={handleDownButtonClicked}
-        className="bg-white hover:bg-gray-200 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
-      >
-        ↓
-      </button>
+        <button
+          onClick={handleDownButtonClicked}
+          className="bg-accent-green hover:bg-green-700 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
+        >
+          ↓
+        </button>
 
-      <button
-        onClick={handleRightButtonClicked}
-        className="bg-white hover:bg-gray-200 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
-      >
-        →
-      </button>
+        <button
+          onClick={handleUpButtonClicked}
+          className="bg-accent-red hover:bg-red-700 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
+        >
+          ↺
+        </button>
+        <button
+          onClick={handleRightButtonClicked}
+          className="bg-white hover:bg-gray-200 text-black w-16 h-16 sprite sprite-shadows cursor-pointer"
+        >
+          →
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,19 +8,21 @@ interface HeaderProps {
 
 const Header = ({ userScore, handleOnPauseClicked, pieceName }: HeaderProps) => {
   return (
-    <div className="w-full p-4 top-0 flex justify-evenly  gap-2 items-center  bg-accent-blue border-b-2 border-black">
-      <button
-        onClick={handleOnPauseClicked}
-        className="cursor-pointer bg-white hover:bg-gray-200 text-black w-13 h-13 text-xs sprite sprite-shadows"
-      >
-        ||
-      </button>
+    <div className="w-full p-4 top-0 flex justify-center bg-accent-blue border-b-2 border-black">
+      <div className="w-full max-w-lg flex justify-evenly gap-2 items-center">
+        <button
+          onClick={handleOnPauseClicked}
+          className="cursor-pointer bg-white hover:bg-gray-200 text-black w-13 h-13 text-xs sprite sprite-shadows"
+        >
+          ||
+        </button>
 
-      <div className="flex-1 h-13 sprite sprite-shadows bg-white flex justify-center items-center text-black text-md capitalize font-black ">
-        Polycases score: {userScore}
+        <div className="flex-1 h-13 sprite sprite-shadows bg-white flex justify-center items-center text-black text-md capitalize font-black ">
+          Polycases score: {userScore}
+        </div>
+
+        <Preview pieceName={pieceName} />
       </div>
-
-      <Preview pieceName={pieceName} />
     </div>
   );
 };
